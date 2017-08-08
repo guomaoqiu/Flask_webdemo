e05aae3f854cd4faaa809c5756b746dfe4a787dc
guomaoqiu <guomaoqiu@gmail.com>
2017-08-08 16:51:22 +0800

    first commit

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,28 @@
 # -*- coding:utf-8 -*-
 from flask import render_template,redirect,request,Response,flash
 from . import main
-import json,commands
 from flask_login import current_user, login_required
-import datetime
-import os
-import sys
+import json,commands,datetime,sys,os
+
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-@login_required
+###############################################################################
+
 @main.route('/')
+@login_required
 def index():
     '''
-    @note: 当前cbt服务器时间
+    @note: 返回主页内容
     '''
     return render_template('index.html')
+
+###############################################################################
+@main.route('/server_list')
+@login_required
+def server_list():
+    '''
+    @note: 主机列表
+    '''
+    return render_template('server_list.html')
