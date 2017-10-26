2abe492a3c815cb953103533d62431cedad0b45d
guomaoqiu <guomaoqiu@gmail.com>
2017-10-26 18:40:23 +0800

    x

--- app/models.py
+++ app/models.py
@@ -7,14 +7,12 @@ from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 
-
 class Permission:
     FOLLOW = 0x01
     COMMENT = 0x02
     WRITE_ARTICLES = 0x04
     MODERATE_COMMENTS = 0x08
     ADMINISTER = 0x80
-
 
 class Role(db.Model):
     __tablename__ = 'roles'
